def60b9806543cea083e259b45e8e1a18ac21df4
Cristo Suárez <cristobits0101@gmail.com>
2026-02-25 20:15:23 +0000

    feat: add LangChain dependencies and installation instructions for a GenAI application.

---
+++ 03-build-genai-app-langchain.py
@@ -0,0 +1,10 @@
+# --- DEPENDENCIAS ---
+# LangChain: Prompts • Memory • Chains • Agents • Tools • RAG • LLMs
+# LangChain Core: PromptTemplate • Runnable/LCEL • ChatModel/LLM • OutputParser
+# LangChain Ollama: llama3.1:latest • mistral:latest • phi3.5:latest
+
+# --- INSTALACIÓN ---
+# pip install -U langchain langchain-core langchain-ollama
+# ollama pull llama3.2:3b
+
+
