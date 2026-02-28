381b8b8964ef6a57aa79cf19d54d7290efb8ad6c
Cristo Suárez <cristobits0101@gmail.com>
2026-02-28 14:37:35 +0000

    add: comentarios de código

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,15 +1,18 @@
 # --- DEPENDENCIAS ---
 
+# Importar el router principal de la API.
 from fastapi import APIRouter
 
-# Importar los routers de cada módulo
+# Importar los routers de cada módulo.
 from .genai.router import router as genai_router
 from .gradio.router import router as gradio_router
 from .llm.router import router as llm_router
 from .prompt.router import router as prompt_router
 from .rag.router import router as rag_router
 
-# Prefijo común para todas las rutas de la API
+# Prefijo común para todas las rutas de la API.
+# Esto ayuda a organizar las rutas y a versionar la API fácilmente.
+# Cada router importado se incluirá bajo este prefijo.
 router = APIRouter(prefix="/api/v1", tags=["API v1"])
 
 router.include_router(genai_router)
