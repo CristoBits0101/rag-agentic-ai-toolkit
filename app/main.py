d1db351054f183513a06860ff15367125df608ff
Cristo Suárez <cristobits0101@gmail.com>
2026-02-28 11:50:14 +0000

    add: comentarios de código

--- app/main.py
+++ app/main.py
@@ -1,22 +1,40 @@
-# Importar el framework FastAPI
+# --- DEPENDENCIAS ---
+
+
+# Importa la clase principal de FastAPI.
 from fastapi import FastAPI
 
-# Importar BaseSettings de pydantic_settings para manejar la configuración de la aplicación
-from pydantic_settings import BaseSettings
+# Importa la herramienta para gestionar configuración.
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+# --- CONFIGURACIÓN ---
+
+
+# Definir un modelo de configuración.
 class Settings(BaseSettings):
-    app_name: str = "AI Agents Dev"
+    # Nombre de la aplicación para la documentación de FastAPI.
+    app_name: str = "RAG and Agentic AI Toolkit"
+    # Configuración para cargar variables de entorno desde un archivo .env
+    model_config = SettingsConfigDict(env_file=".env")
 
-    class Config:
-        env_file = ".env"
 
-
+# Crea una instancia de la configuración.
 settings = Settings()
-
+# Crea una instancia de la aplicación FastAPI utilizando el nombre definido en la configuración.
 app = FastAPI(title=settings.app_name)
 
 
-@app.get("/health")
-async def health():
-    return {"status": "dev running"}
+# --- RUTAS ---
+
+
+# Ruta raíz para verificar que la aplicación está funcionando y para mostrar el nombre del servicio.
+@app.get("/", tags=["Root"])
+async def root():
+    return {"service": settings.app_name}
+
+
+# Ruta útil para verificar la salud del servicio al monitoreo y despliegue.
+@app.get("/health", tags=["Health"])
+async def health_check():
+    return {"status": "ok"}
